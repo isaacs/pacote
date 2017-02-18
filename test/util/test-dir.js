0b88468587562b2603d79880f6fdbd64d8e00e86
isaacs <i@izs.me>
2017-02-18 14:23:56 -0800

    what if rimraf, but TOO MUCH?

--- test/util/test-dir.js
+++ test/util/test-dir.js
@@ -5,6 +5,23 @@ var path = require('path')
 var rimraf = require('rimraf')
 var tap = require('tap')
 
+var rmtries = process.platform === 'win32' ? 100 : 1
+
+function rimrafSync (dir) {
+  var i = 0
+  do {
+    i++
+    try {
+      return rimraf.sync(dir)
+    } finally {
+      if (i < rmtries) {
+        continue
+      }
+    }
+  } while (true)
+}
+
+
 var cacheDir = path.resolve(__dirname, '../cache')
 
 module.exports = testDir
@@ -15,7 +32,7 @@ function testDir (filename) {
   if (!process.env.KEEPCACHE) {
     tap.tearDown(function () {
       process.chdir(__dirname)
-      rimraf.sync(dir)
+      rimrafSync(dir)
     })
     tap.afterEach(function (cb) {
       reset(dir)
@@ -28,7 +45,7 @@ function testDir (filename) {
 module.exports.reset = reset
 function reset (testDir) {
   process.chdir(__dirname)
-  rimraf.sync(testDir)
+  rimrafSync(testDir)
   mkdirp.sync(testDir)
   process.chdir(testDir)
 }
