a97f61328bf87f75002bea0afb1d23663cda3901
isaacs <i@izs.me>
2017-02-18 14:08:33 -0800

    use latest more aggressive rimraf, and tap's master branch

--- test/util/test-dir.js
+++ test/util/test-dir.js
@@ -5,23 +5,6 @@ var path = require('path')
 var rimraf = require('rimraf')
 var tap = require('tap')
 
-var rmtries = process.platform === 'win32' ? 100 : 1
-
-function rimrafSync (dir) {
-  var i = 0
-  do {
-    i++
-    try {
-      return rimraf.sync(dir)
-    } finally {
-      if (i < rmtries) {
-        continue
-      }
-    }
-  } while (true)
-}
-
-
 var cacheDir = path.resolve(__dirname, '../cache')
 
 module.exports = testDir
@@ -32,7 +15,7 @@ function testDir (filename) {
   if (!process.env.KEEPCACHE) {
     tap.tearDown(function () {
       process.chdir(__dirname)
-      rimrafSync(dir)
+      rimraf.sync(dir)
     })
     tap.afterEach(function (cb) {
       reset(dir)
@@ -45,7 +28,7 @@ function testDir (filename) {
 module.exports.reset = reset
 function reset (testDir) {
   process.chdir(__dirname)
-  rimrafSync(testDir)
+  rimraf.sync(testDir)
   mkdirp.sync(testDir)
   process.chdir(testDir)
 }
