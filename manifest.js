a9d99ed2809c6af7bb097b43d216ac1aa5cd7b35
isaacs <i@izs.me>
2017-02-18 09:38:45 -0800

    add logging around manifest fetcher

--- manifest.js
+++ manifest.js
@@ -14,12 +14,15 @@ function manifest (spec, opts, cb) {
   opts = optCheck(opts)
 
   rps(spec, function (err, res) {
+    console.log('rps cb', er, res)
     if (err) { return cb(err) }
     if (!handlers[res.type]) {
       handlers[res.type] = require('./lib/handlers/' + res.type + '/manifest')
     }
     var fetcher = handlers[res.type]
+    console.log('>>>fetcher', res.type)
     fetcher(res, opts, function (err, mani) {
+      console.log('<<<fetcher', res.type, er, mani)
       cb(err, mani)
     })
   })
