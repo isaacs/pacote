d9a43e7515c71e055a6cd84884d61e43e9c2ac81
isaacs <i@izs.me>
2017-02-18 10:20:38 -0800

    logs to try to find where it falls off the event loop

--- manifest.js
+++ manifest.js
@@ -14,15 +14,15 @@ function manifest (spec, opts, cb) {
   opts = optCheck(opts)
 
   rps(spec, function (err, res) {
-    console.log('rps cb', err, res)
+    console.log('rps cb', spec, err, res)
     if (err) { return cb(err) }
     if (!handlers[res.type]) {
       handlers[res.type] = require('./lib/handlers/' + res.type + '/manifest')
     }
     var fetcher = handlers[res.type]
-    console.log('>>>fetcher', res.type)
+    console.log('>>>fetcher', spec, res.type)
     fetcher(res, opts, function (err, mani) {
-      console.log('<<<fetcher', res.type, err, mani)
+      console.log('<<<fetcher', spec, res.type, err, mani)
       cb(err, mani)
     })
   })
